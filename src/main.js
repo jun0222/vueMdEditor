9f8082ca3ae487b7a01ad4570df7bd7050602885
JunHatanaka <nextseason6@gmail.com>
2022-07-15 00:31:30 +0900

    Add: 1-2まで完了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify"; // ←ここに追加
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify, // ←ここに追加
+  render: (h) => h(App),
+}).$mount("#app");
