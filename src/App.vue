2769b304c1e1950c22f5e9403d2972be92c82576
JunHatanaka <nextseason6@gmail.com>
2023-01-27 01:28:12 +0900

    Feature: 日本語化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="main">
-      <mavon-editor v-model="value" language="en" font-size="24px" />
+      <mavon-editor v-model="value" language="ja" font-size="24px" />
     </div>
   </div>
 </template>
